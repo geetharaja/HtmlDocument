e66958acfd6b26b0bbf8297026591215e149f337
geetharaja <vsgeetha1991@gmail.com>
2022-05-26 16:11:11 -0700

    This is my second commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Learning javascript</h2>
+<p id="demo">Javascript can change Html</p>
+<button type="button" onclick='document.getElementById("demo").innerHTML="Hello Javascript"'>Click me</button
+>
+</body>
+</html>
